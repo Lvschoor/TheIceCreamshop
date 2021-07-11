635f3d55e1b5ed0b2b6103de1392996cf0756828
Lvschoor <lvschoor@gmail.com>
2021-07-11 16:08:51 +0200

    Started option to order ice cream. Next TODO exception handling when out of stock.

--- src/be/intecbrussel/application/IceCreamAppFX.java
+++ src/be/intecbrussel/application/IceCreamAppFX.java
@@ -10,6 +10,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 
@@ -28,21 +29,21 @@ public class IceCreamAppFX extends Application {
         Controller controller = new Controller(iceCreamCar, stock, priceList);
         loader.setController(controller);
         Parent root = loader.load();
-        //TextField conesUpdate = new TextField();
 
 
 // setting stage title, and output to console when stage is shown
-        primaryStage.setTitle("Welcome to the Ice Cream Shop");
+        primaryStage.setTitle(" Welcome to the Ice Cream Shop");
+        Image icon = new Image(getClass().getResourceAsStream("icon.png"));
+        primaryStage.getIcons().add(icon);
         primaryStage.setOnShowing((event) -> {
             System.out.println("Showing Stage");
         });
 //creating Scene on stage
         primaryStage.setScene(new Scene(root, 500, 300));
+
 // showing scene
         primaryStage.show();
 
-        //getting stock info
-        //stock.setCones(conesUpdate.getText());
 //output to console to confirm closing stage
         primaryStage.setOnCloseRequest((event) -> {
             System.out.println("Closing Stage");
--- src/be/intecbrussel/controller/Controller.java
+++ src/be/intecbrussel/controller/Controller.java
@@ -1,14 +1,13 @@
 package be.intecbrussel.controller;
 
+import be.intecbrussel.exceptions.NoMoreIceCreamException;
 import be.intecbrussel.sellers.IceCreamSeller;
 import be.intecbrussel.sellers.PriceList;
 import be.intecbrussel.sellers.Stock;
-import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.SplitPane;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.scene.control.Button;
@@ -19,12 +18,17 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 
-public class Controller extends VBox implements Initializable{
+public class Controller extends VBox implements Initializable {
 
     @FXML
     private Stock stock;
+    @FXML
     private PriceList pricelist;
+    @FXML
     private IceCreamSeller iceCreamCar;
+    @FXML
+    private int items=0;
+
 
     public Controller(IceCreamSeller iceCreamCar, Stock stock, PriceList pricelist) {
         this.iceCreamCar = iceCreamCar;
@@ -45,13 +49,26 @@ public class Controller extends VBox implements Initializable{
     private Text text2;
 
     @FXML
-    private Button button1;
+    private Button stockStatus;
 
     @FXML
-    private Button button2;
+    private Button updateStock;
 
     @FXML
-    private Button button3;
+    private Button submitEntries;
+
+    @FXML
+
+    private Button orderIceCream;
+
+    @FXML
+    private Button iceRocket;
+
+    @FXML
+    private Button magnum;
+
+    @FXML
+    private Button cone;
 
     @FXML
     private Text cones;
@@ -78,6 +95,9 @@ public class Controller extends VBox implements Initializable{
     private Text magni2;
 
     @FXML
+    private Text itemsOrdered;
+
+    @FXML
     private TextField conesUpdate;
 
     @FXML
@@ -102,13 +122,11 @@ public class Controller extends VBox implements Initializable{
     private TextFlow updateMagni;
 
 
-
-
     // when button is clicked, show stock
     @FXML
-    public void button1Clicked(Event e) {
-        System.out.println("Button 1 clicked");
-        button1.setText("Refresh stock status");
+    public void stockStatusClicked() {
+        System.out.println("Stock status button clicked");
+        stockStatus.setText("Refresh stock status");
         text1.setVisible(true);
         text2.setVisible(false);
         infoFields.setVisible(true);
@@ -116,9 +134,8 @@ public class Controller extends VBox implements Initializable{
         updateBalls.setVisible(false);
         updateIceRockets.setVisible(false);
         updateMagni.setVisible(false);
-        button2.setVisible(true);
-        button3.setVisible(false);
-
+        updateStock.setVisible(true);
+        submitEntries.setVisible(false);
         cones.setText("Cones: " + Integer.toString(stock.getCones()));
         balls.setText("Ice cream balls: " + Integer.toString(stock.getBalls()));
         iceRockets.setText("Ice Rockets: " + Integer.toString(stock.getIceRockets()));
@@ -126,36 +143,70 @@ public class Controller extends VBox implements Initializable{
     }
 
     @FXML
-    public void button2Clicked(Event e2) {
-        System.out.println("Button 2 clicked");
-        button2.setVisible(false);
+    public void updateStockClicked() {
+        System.out.println("Update stock button clicked");
+        updateStock.setVisible(false);
         infoFields.setVisible(true);
         text2.setVisible(true);
         updateCones.setVisible(true);
         updateBalls.setVisible(true);
         updateIceRockets.setVisible(true);
         updateMagni.setVisible(true);
-        button3.setVisible(true);
+        submitEntries.setVisible(true);
         conesUpdate.setText(String.valueOf(stock.getCones()));
         ballsUpdate.setText(String.valueOf(stock.getBalls()));
         iceRocketsUpdate.setText(String.valueOf(stock.getIceRockets()));
         magniUpdate.setText(String.valueOf(stock.getMagni()));
 
     }
+
     @FXML
-    public void button3Clicked(Event e3) {
-        System.out.println("Button 3 clicked");
-        System.out.println(conesUpdate.getText());
+    public void submitEntriesClicked() {
+        System.out.println("Submit entries button clicked");
         stock.setCones(Integer.parseInt(conesUpdate.getText()));
         stock.setBalls(Integer.parseInt(ballsUpdate.getText()));
         stock.setIceRockets(Integer.parseInt(iceRocketsUpdate.getText()));
         stock.setMagni(Integer.parseInt(magniUpdate.getText()));
-
-        button2.setVisible(true);
-
+        text2.setVisible(false);
+        updateCones.setVisible(false);
+        updateBalls.setVisible(false);
+        updateIceRockets.setVisible(false);
+        updateMagni.setVisible(false);
+        updateStock.setVisible(true);
+        submitEntries.setVisible(false);
 
     }
 
+    @FXML
+    public void orderIceCreamClicked() {
+        System.out.println("Order Ice Cream button clicked");
+        infoFields.setVisible(true);
+
+        iceRocket.setVisible(true);
+        magnum.setVisible(true);
+        cone.setVisible(true);
+        itemsOrdered.setVisible(true);
+    }
+
+    @FXML
+    public void iceRocketClicked() throws NoMoreIceCreamException {
+        System.out.println("Ice Rocket button clicked");
+        iceCreamCar.orderIceRocket();
+        items++;
+        itemsOrdered.setText("Items ordered: "+String.valueOf(items));
+
+
+    }
+    @FXML
+    public void magnumClicked() {
+        System.out.println("Magnum button clicked");
+
+    }
+    @FXML
+    public void coneClicked() {
+        System.out.println("Cone button clicked");
+
+    }
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
     }
